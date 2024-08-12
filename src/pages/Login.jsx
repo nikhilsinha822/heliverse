3006115d9c4f8eec73d456734b86d271424a12eb
nikhilsinha822 <nikhilsinha822@gmail.com>
2024-08-12 12:11:30 +0530

    added login page

--- src/pages/Login.jsx
+++ src/pages/Login.jsx
@@ -1,9 +1,99 @@
-const Login = () => {
-  return (
-    <h1 className="text-3xl font-bold underline">
-      Hello world!
-    </h1>
-  )
-}
+import { useState } from 'react';
+import { FaUser, FaLock, FaSchool } from 'react-icons/fa';
+import axios from 'axios'
+import { useNavigate } from 'react-router-dom';
 
-export default Login
+const Login = () => {
+  const [email, setEmail] = useState('');
+  const [password, setPassword] = useState('');
+  const [error, setError] = useState('');
+  const navigate = useNavigate();
+
+  const handleSubmit = async (e) => {
+    e.preventDefault();
+    try {
+      const response = await axios.post(`${import.meta.env.VITE_BASE_URL}/auth/login`, {
+        email, password
+      })
+      navigate(`/${response.data.data.redirect}`)
+      setError('');
+      setEmail('');
+      setPassword('');
+    } catch (error) {
+      setError(error.response.data.message);
+    }
+  };
+
+  return (
+    <div className="min-h-screen bg-gray-100 flex items-center justify-center">
+      <div className="max-w-md w-full space-y-8 p-10 bg-white rounded-xl shadow-md">
+        <div className="text-center">
+          <FaSchool className="mx-auto h-12 w-12 text-indigo-600" />
+          <h2 className="mt-6 text-3xl font-extrabold text-gray-900">
+            Classroom Management System
+          </h2>
+          <p className="mt-2 text-sm text-gray-600">Sign in to your account</p>
+        </div>
+        <form className="mt-8 space-y-6" onSubmit={handleSubmit}>
+          <div className="rounded-md shadow-sm -space-y-px">
+            <div>
+              <label htmlFor="email" className="sr-only">
+                email
+              </label>
+              <div className="relative">
+                <div className="absolute inset-y-0 left-0 pl-3 flex items-center pointer-events-none">
+                  <FaUser className="h-5 w-5 text-gray-400" />
+                </div>
+                <input
+                  id="email"
+                  name="email"
+                  type="text"
+                  required
+                  className="appearance-none relative block w-full my-2 px-3 py-2 pl-10 border border-gray-300 placeholder-gray-500 text-gray-900 rounded-md focus:outline-none focus:ring-2 focus:ring-indigo-500 focus:border-indigo-500 sm:text-sm"
+                  placeholder="Email"
+                  value={email}
+                  onChange={(e) => setEmail(e.target.value)}
+                />
+              </div>
+            </div>
+            <div>
+              <label htmlFor="password" className="sr-only">
+                Password
+              </label>
+              <div className="relative">
+                <div className="absolute inset-y-0 left-0 pl-3 flex items-center pointer-events-none">
+                  <FaLock className="h-5 w-5 text-gray-400" />
+                </div>
+                <input
+                  id="password"
+                  name="password"
+                  type="password"
+                  required
+                  className="appearance-none relative block w-full px-3 py-2 pl-10 border border-gray-300 placeholder-gray-500 text-gray-900 rounded-md focus:outline-none focus:ring-2 focus:ring-indigo-500 focus:border-indigo-500 sm:text-sm"
+                  placeholder="Password"
+                  value={password}
+                  onChange={(e) => setPassword(e.target.value)}
+                />
+              </div>
+            </div>
+          </div>
+
+          <div>
+            <button
+              type="submit"
+              className="group relative w-full flex justify-center py-2 px-4 border border-transparent text-sm font-medium rounded-md text-white bg-indigo-600 hover:bg-indigo-700 focus:outline-none focus:ring-2 focus:ring-offset-2 focus:ring-indigo-500"
+            >
+              Sign in
+            </button>
+          </div>
+        </form>
+        {error && <div className="bg-red-100 border text-sm flex gap-1 border-red-400 text-red-700 px-4 py-3 my-3 rounded relative" role="alert">
+          <strong className="font-bold">Error!</strong>
+          <span className="block sm:inline"> {error}</span>
+        </div>}
+      </div>
+    </div>
+  );
+};
+
+export default Login;
--- src/pages/principal/index.jsx
+++ src/pages/principal/index.jsx
@@ -1,4 +1,15 @@
+import axios from "axios";
+import { useEffect } from "react";
+
+
 const PrincipalDash = () => {
+    useEffect(() => {
+        const fetchClassroom = async () => {
+            const response = await axios.get(`${import.meta.env.VITE_BASE_URL}/api/classroom`)
+            console.log(response.data.data)
+        }
+        fetchClassroom();
+    }, [])
     return (
         <div>Principal</div>
     );
